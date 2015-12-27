4240c388d9a77f5a854ed7f2989eadab8533001f
Sinuhé Coronel <lesmo@lesmo.com.mx>
2015-12-27 17:48:05 -0600

    EGI reached Alpha

---
+++ src/inbox.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// This absolutely does not share the Node.js context
+// it'll be injected into Inbox DOM using webContent.executeJavaScript'
+module.exports = function(window) {
+      
+};
--- src/main.js
+++ src/main.js
@@ -3,15 +3,20 @@ process.env.ELECTRON_HIDE_INTERNAL_MODULES = 'true';
 
 var electron = require('electron');
 var app = electron.app;
-var BrowserWindow = electron.BrowserWindow;
-
-var mainWindow = null;
 
 app.on('window-all-closed', function() {
     if ( process.platform !== 'darwin' )
         app.quit();
 });
 
+var BrowserWindow = electron.BrowserWindow;
+var shell = require('shell');
+var file = require('fs');
+var sessionWindows = [];
+var inboxInject = {
+    js: '(' + require(__dirname + '/inbox.js').toString() + ')(window);',
+    css: file.readFileSync(__dirname + '/inbox.css', 'utf8')
+};
 var startupOpts = {
     useContentSize: true,
     width: 940,
@@ -20,24 +25,57 @@ var startupOpts = {
     resizable: true,
     alwaysOnTop: false,
     fullscreen: false,
-    skipTaskbar: true,
+    skipTaskbar: false,
     kiosk: false,
     title: 'Inbox',
     icon: null,
     show: false,
     frame: true,
     disableAutoHideCursor: false,
-    autoHideMenuBar: false,
-    titleBarStyle: 'default'
+    titleBarStyle: 'default',
+    directWrite: true,
+    preload: 'inbox.js',
+    nodeIntegration: false // for security reasons
 };
 
-app.on('ready', function() {
-    mainWindow = new BrowserWindow(startupOpts);
+function openWindow(url, openDevTools) {
+    var window = sessionWindows[sessionWindows.length] = new BrowserWindow(startupOpts);
     
-    mainWindow.loadURL('https://inbox.google.com');
-    mainWindow.on('closed', function() {
-        mainWindow = null;
+    window.setMenuBarVisibility(false);
+    window.loadURL(url);
+    
+    window.on('page-title-updated', function() {
+        var titleEmailMatch = this.getTitle().match(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/i);
+        
+        window.email = titleEmailMatch
+            ? titleEmailMatch[0].toLowerCase()
+            : null;
     });
     
-    mainWindow.show();
+    window.on('closed', function() {
+        var i = sessionWindows.indexOf(window);
+        sessionWindows.splice(i, 1);
+    });
+    
+    window.webContents.on('dom-ready', function() {
+        window.webContents.executeJavaScript(inboxInject.js);
+        window.webContents.insertCSS(inboxInject.css);
+    });
+    
+    window.webContents.on('new-window', function(e, url) {
+        e.preventDefault();
+        
+        // Don't open new Inbox windows on browser
+        if ( url.match(/https?:\/\/(accounts|inbox)\.google\.com|inbox/i) )
+            openWindow(url);
+        else
+            shell.openExternal(url);
+    });
+    
+    window.show();
+    return window;
+}
+
+app.on('ready', function() {
+    openWindow('https://inbox.google.com');
 });
